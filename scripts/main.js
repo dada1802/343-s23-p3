8b0a5596a6073ff539c84a6700fae1625e42cd6a
David Nguyen <nguy26dx@dukes.jmu.edu>
2023-05-01 16:57:59 -0400

    UpdateD

--- scripts/main.js
+++ scripts/main.js
@@ -4,44 +4,23 @@ searchForm.onsubmit = (ev) => {
   console.log("submitted top-search with", ev);
   ev.preventDefault();
   foodUL.innerHTML = "";
+  getTime();
   // https://stackoverflow.com/a/26892365/1449799
   const formData = new FormData(ev.target);
-  // console.log(formData)
-  // for (const pair of formData.entries()) {
-  //   console.log(`${pair[0]}, ${pair[1]}`);
-  // }
+  
   const queryText = formData.get("query");
   // console.log("queryText", queryText);
 
 
   const recipes = getRecipes(queryText);
-  // console.log(recipes);
   recipes.then((recipe) => {
     const mealsArray = recipe.meals;
-    // console.log(recipe.meals);
-    // console.log(mealsArray);
-    const listItems = mealsArray.map(createElem);
+    const listItems = mealsArray.map(createFood);
     listItems.forEach((item) => {
       foodUL.appendChild(item);
     });
   });
-
-
-  // const rhymeResultsPromise = getRhymes(queryText);
-  // console.log(rhymeResultsPromise);
-  // rhymeResultsPromise.then((rhymeResults) => {
-  //   const rhymeListItemsArray = rhymeResults.map(rhymObj2DOMObj);
-    // console.log("rhymeListItemsArray", rhymeListItemsArray);
-    // const rhymeResultsUL = document.getElementById("rhyme-results");
-    // rhymeListItemsArray.forEach((rhymeLi) => {
-    //   rhymeResultsUL.appendChild(rhymeLi);
-    // });
-  // });
 };
-
-// given a word (string), search for rhymes
-// https://rhymebrain.com/api.html#rhyme
-//  https://rhymebrain.com/talk?function=getRhymes&word=hello
 
 const getRhymes = (word) => {
   console.log("attempting to get rhymes for", word);
@@ -51,57 +30,19 @@ const getRhymes = (word) => {
 };
 
 const getRecipes = (word) => {
-  // console.log("Trying to get recipe for", word);
   return fetch(
     `https://themealdb.com/api/json/v1/1/filter.php?a=${word}&apiKey=1`
   ).then((resp) => resp.json());
 };
 
-// const getSong = (word) => {
-//   return fetch(
-//     `https://shazam.p.rapidapi.com/v1/charts/genres/${word}`
-//   ).then((resp) => resp.json());
-// };
-
-// `https://food2fork.ca/api/recipe/search/?page=2&query=${food}`
-
-
-const rhymObj2DOMObj = (rhymeObj) => {
-  //this should be an array where each element has a structure like
-  //
-  // "word": "no",
-  // "frequency": 28,
-  // "score": "300",
-  // "flags": "bc",
-  // "syllables": "1"
-  const rhymeListItem = document.createElement("li");
-  const rhymeButton = document.createElement("button");
-  rhymeButton.classList.add('btn')
-  rhymeButton.classList.add('btn-info')
-  rhymeButton.textContent = rhymeObj.word;
-  rhymeButton.onclick = searchForBook;
-  rhymeListItem.appendChild(rhymeButton);
-  return rhymeListItem;
-};
-
-const createElem = (recipe) => {
-  // const foodList = document.createElement("li");
+const createFood = (recipe) => {
   const foodList = document.createElement("div");
   const img = document.createElement("img");
-  const name = document.createElement("button");
+  const name = document.createElement("p");
 
   name.textContent = recipe.strMeal;
-  name.onclick = searchForRecipe;
-  // anchor.href = "https://www.google.com";
-  // anchor.href = searchForRecipe(recipe.strMeal);
-  // const test = searchForRecipe(recipe.strMeal);
-  // console.log(test);  
-
-  // here
-  // anchor.innerText = recipe.strMeal;
   foodList.appendChild(img);
   
-  // foodList.appendChild(anchor);
   foodList.appendChild(name);
   img.src = recipe.strMealThumb;
   img.width = 300;
@@ -109,65 +50,15 @@ const createElem = (recipe) => {
   return foodList;
 };
 
-const searchForRecipe = (ev) => {
-  const word = ev.target.textContent;
-  console.log(word);
-  return fetch(
-    `https://recipe-puppy.p.rapidapi.com/?q=${word}`
-  ).then((resp) => resp.json()
+const getTime = () => {
+  const url = `http://worldtimeapi.org/api/ip`;
   
-  );
+  return fetch(url).then((resp) => resp.json()).then(data => {
+    const datetime = new Date(data.datetime);
+    const date = datetime.toLocaleDateString();
+    const time = datetime.toLocaleTimeString();
+    const timeOfDay = document.getElementById("timeOfDay");
+    timeOfDay.innerText = `The current date is: ${date}\n The current time is: ${time}`;
+  });
 };
 
-const searchForBook = (ev) => {
-  const word = ev.target.textContent;
-  console.log("search for", word);
-  return fetch(`https://gutendex.com/books/?search=${word}`).then((r) =>
-    r.json()
-  ).then((bookResultsObj)=> {
-    // console.log(bookResultsObj.hasOwnProperty('results'))
-    const bookCardsArray = bookResultsObj.results.map(songObj2DOMObj)
-    console.log("bookCardsArray", bookCardsArray);
-    const bookResultsElem = document.getElementById("book-results");
-    bookCardsArray.forEach(book=>bookResultsElem.appendChild(book))
-  })
-};
-
-const songObj2DOMObj = (bookObj) => {
-  // {"id":70252,"title":"Threads gathered up : $b A sequel to \"Virgie's Inheritance\"","authors":[{"name":"Sheldon, Georgie, Mrs.","birth_year":1843,"death_year":1926}],"translators":[],"subjects":["American fiction -- 19th century"],"bookshelves":[],"languages":["en"],"copyright":false,"media_type":"Text","formats":{"image/jpeg":"https://www.gutenberg.org/cache/epub/70252/pg70252.cover.medium.jpg","application/rdf+xml":"https://www.gutenberg.org/ebooks/70252.rdf","text/plain":"https://www.gutenberg.org/ebooks/70252.txt.utf-8","application/x-mobipocket-ebook":"https://www.gutenberg.org/ebooks/70252.kf8.images","application/epub+zip":"https://www.gutenberg.org/ebooks/70252.epub3.images","text/html":"https://www.gutenberg.org/ebooks/70252.html.images","application/octet-stream":"https://www.gutenberg.org/files/70252/70252-0.zip","text/plain; charset=us-ascii":"https://www.gutenberg.org/files/70252/70252-0.txt"},"download_count":745},
-
-  // make a dom element
-  // add bookObj.title to the element
-  // return element
-
-  const bookCardDiv = document.createElement("div");
-  bookCardDiv.classList.add("card");
-
-  const bookCardBody = document.createElement("div");
-  bookCardBody.classList.add("card-body");
-
-  const titleElem = document.createElement("h5");
-  titleElem.textContent = bookObj.title;
-  bookCardBody.appendChild(titleElem);
-  const cardText = document.createElement("p");
-  cardText.textContent =
-    "Some quick example text to build on the card title and make up the bulk of the card's content.";
-  bookCardBody.appendChild(cardText);
-  if (bookObj?.formats?.["image/jpeg"]) {
-    const bookCardImg = document.createElement("img");
-    bookCardImg.classList.add("card-img-top");
-    bookCardImg.src = bookObj?.formats?.["image/jpeg"];
-    bookCardBody.appendChild(bookCardImg)
-  }
-  if (bookObj?.formats?.["text/plain"]) {
-    const bookTextLink = document.createElement("a");
-    bookTextLink.href = bookObj?.formats?.["text/plain"];
-    bookTextLink.classList.add("btn");
-    bookTextLink.classList.add("btn-primary");
-    bookTextLink.textContent = "Read It!";
-    bookCardBody.appendChild(bookTextLink);
-  }
-  bookCardDiv.appendChild(bookCardBody)
-  return bookCardDiv
-  
-};
